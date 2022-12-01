df079bdb7d8df674f01c36e891d6da56945d5b29
PrajaktaShewale <shewalep94@gmail.com>
2022-12-01 13:16:49 +0530

    [Prajakta]Add:employee Wage Problem

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random randomObj = new Random();
+            int checkPresent = randomObj.Next(0,2);
+            Console.WriteLine("Random Number is " + checkPresent);
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
