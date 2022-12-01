f9014488af4b4c7829e18d6024a594d2ca77bc8b
PrajaktaShewale <shewalep94@gmail.com>
2022-12-01 15:58:03 +0530

    [Prajakta]Add:Finding Emp Wage Using switch Statement

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,27 +4,33 @@ namespace EmployeeWage
 {
     internal class Program
     {
+        public const int fullTime = 1, partTime = 2, Emp_Rate_Per_Hrs = 20;
         static void Main(string[] args)
         {
-            int fullTime = 1,partTime = 2,Emp_Rate_Per_Hrs = 20,empHrs = 0,empWage=0;
+            int empHrs = 0, empWage = 0;
             Random randomObj = new Random();
-            int checkPresent = randomObj.Next(0,3);
+            int checkPresent = randomObj.Next(0, 3);
             Console.WriteLine("Random Number is " + checkPresent);
-            if (checkPresent == fullTime)
+
+            switch (checkPresent)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is Present");
+                case fullTime:
+
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Present");
+                    break;
+
+                case partTime:
+                    empWage = 4;
+                    Console.WriteLine("Employee is Present");
+                    break;
+                default:
+                    empWage = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
-            else if (checkPresent == partTime)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
-            }
+
+
             empWage = empHrs * Emp_Rate_Per_Hrs;
             Console.WriteLine("Emp Wage :" + empWage);
         }
