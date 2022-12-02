75afdacd0993b675b55375787b2fadb9b3375d9e
PrajaktaShewale <shewalep94@gmail.com>
2022-12-02 10:58:46 +0530

    [Prajakta]Add:Finding monthly Emp Wage for 20 days per month

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,35 +4,41 @@ namespace EmployeeWage
 {
     internal class Program
     {
-        public const int fullTime = 1, partTime = 2, Emp_Rate_Per_Hrs = 20;
+        public const int fullTime = 1, partTime = 2, Emp_Rate_Per_Hrs = 20, numWorkingDays = 20;
         static void Main(string[] args)
         {
-            int empHrs = 0, empWage = 0;
-            Random randomObj = new Random();
-            int checkPresent = randomObj.Next(0, 3);
-            Console.WriteLine("Random Number is " + checkPresent);
-
-            switch (checkPresent)
+            int empHrs = 0, empWage = 0, monthlyempWage = 0;
+            for (int day = 0; day < numWorkingDays; day++)
             {
-                case fullTime:
+                Random randomObj = new Random();
+                int checkPresent = randomObj.Next(0, 3);
+                Console.WriteLine("Random Number is " + checkPresent);
 
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Present");
-                    break;
+                switch (checkPresent)
+                {
+                    case fullTime:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is Present");
+                        break;
 
-                case partTime:
-                    empWage = 4;
-                    Console.WriteLine("Employee is Present");
-                    break;
-                default:
-                    empWage = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                    case partTime:
+                        empWage = 4;
+                        Console.WriteLine("Employee is Present");
+                        break;
+
+                    default:
+                        empWage = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+
+
+                empWage = empHrs * Emp_Rate_Per_Hrs;
+                monthlyempWage += empWage;
+                Console.WriteLine("Emp Wage :" + empWage);
             }
-
-
-            empWage = empHrs * Emp_Rate_Per_Hrs;
-            Console.WriteLine("Emp Wage :" + empWage);
+            monthlyempWage = empWage * numWorkingDays;
+            Console.WriteLine("Monthly Emp Wage :" + monthlyempWage);
         }
     }
 }
