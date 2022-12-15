e03730ed75d60bb2225f0497120dc2d6e7699824
PrajaktaShewale <shewalep94@gmail.com>
2022-12-15 16:46:03 +0530

    [Prajakta]Add:Compute Employee Wage for Multiple Companies

---
+++ EmployeeWage/EmpWageMultipleCompanies.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpWageMultipleCompanies
+    {
+        public const int fullTime = 1, partTime = 2;
+        public void EmpWageMultiple(string Company, int Emp_Rate_Per_Hrs, int numWorkingDays, int MaxHrsInMonth)
+        {
+            int empHrs = 0, empWage = 0, TotalWorkingDays = 0, totalEmpHrs = 0;
+
+            while (totalEmpHrs <= MaxHrsInMonth && TotalWorkingDays < numWorkingDays)
+            {
+                TotalWorkingDays++;
+                Random randomObj = new Random();
+                int checkPresent = randomObj.Next(0, 3);
+                //Console.WriteLine("Random Number is " + checkPresent);
+
+                switch (checkPresent)
+                {
+                    case fullTime:
+                        empHrs = 8;
+                      //  Console.WriteLine("Employee is Present");
+                        break;
+
+                    case partTime:
+                        empHrs = 4;
+                       // Console.WriteLine("Employee is Present");
+                        break;
+
+                    default:
+                        empHrs = 0;
+                       // Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                //Console.WriteLine("Days :" + TotalWorkingDays + "EmployeeHrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hrs;
+            Console.WriteLine("Company Name :- "+Company+" And Total Wage Of Emplyoee:" + totalEmpWage);
+        }
+    }    
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,10 +9,12 @@ namespace EmployeeWage
             bool flag = true;
             while (flag)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("employee Wage Problem");
+                Console.ResetColor();
                 Console.WriteLine("Enter a number to get perticular UC");
                 Console.WriteLine("1.Attendance\n2.Calculate Daily EMP Wage\n3.Add Part Time EMP Wage\n4.Calculate EMP Wage Using Switch statement" +
-                    "\n5.Calculate Wages For A month\n6.Calculate till hrs and Condition Apply");
+                    "\n5.Calculate Wages For A month\n6.Calculate till hrs and Condition Apply\n7.Multiple Companies");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch(check)
                 {
@@ -39,6 +41,11 @@ namespace EmployeeWage
                     case 6:
                         CalWagesTillConditionUC6 uc6 = new CalWagesTillConditionUC6();
                         uc6.DayMonthCondition();
+                        break;
+                    case 7:
+                        EmpWageMultipleCompanies uc8 = new EmpWageMultipleCompanies();
+                        uc8.EmpWageMultiple("TATA", 10, 20, 100);
+                        uc8.EmpWageMultiple("Reliance", 20, 20, 80);
                         break;
                         default:
                         flag = false;
